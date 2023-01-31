ff35a8388429ead6713f914e493577417644204e
NikiRufolo <nikirufolo@gmail.com>
2023-01-31 16:45:35 +0100

    print pic

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="text-center">
-        <h1>{{$singolo_post->title}}</h1>
-        <p>{{$singolo_post->body}}</p>
+        <h1>{{ $singolo_post->title }}</h1>
+        <img src="{{ asset("storage/$singolo_post->cover") }}"alt="">
+        <p>{{ $singolo_post->body }}</p>
     </div>
 @endsection
